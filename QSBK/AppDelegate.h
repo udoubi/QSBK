b1fdef02057a44cce682b78398b21314385d2b8f
王帆 <tianyu@heyjude.local>
2016-06-03 15:42:37 +0800

    Initial Commit

---
+++ QSBK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QSBK
+//
+//  Created by jiangke on 16/6/3.
+//  Copyright © 2016年 TianYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
